0ecdc6b06ccb8e814fa1dba4a74ee08603c52b34
reyesjl <reyesjl.nine@hotmail.com>
2024-01-16 14:14:12 -0500

    add landing page with base options;

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Tournament, Hotel, HotelPositive, HotelProblem, Review
 
-# Register your models here.
+admin.site.register(Tournament)
+admin.site.register(Hotel)
+admin.site.register(HotelPositive)
+admin.site.register(HotelProblem)
+admin.site.register(Review)
