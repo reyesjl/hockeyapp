ea9269238b66d955fbb56516edcadd439daefc76
Jose <reyesjl.nine@hotmail.com>
2024-01-31 16:29:34 +0000

    add restaurant basic model;

--- main/settings.py
+++ main/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tournaments',
+    'restaurants',
     'submissions',
     'locations',
     'reviews',
--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from locations.models import Location
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    rating_overall = models.DecimalField(default=1.0, max_digits=3, decimal_places=1, validators=[MinValueValidator(1.0), MaxValueValidator(5.0)])
