f024308eaa2d99888fe14e48adcb11dac6b481e6
Jose <reyesjl.nine@hotmail.com>
2024-02-01 00:06:35 +0000

    restaurants by tournament work;

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from locations.models import Location
+from tournaments.models import Tournament
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     rating_overall = models.DecimalField(default=1.0, max_digits=3, decimal_places=1, validators=[MinValueValidator(1.0), MaxValueValidator(5.0)])
+
+class RestaurantMetadata(models.Model):
+    restaurant = models.OneToOneField(Restaurant, on_delete=models.CASCADE, unique=True)
+    
+    class Meta:
+        unique_together = ('restaurant',)
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,2 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Restaurant
+from tournaments.models import Tournament
 
+def restaurants_by_tournament(request, tournament_id):
+    restaurants = Restaurant.objects.filter(tournament_id=tournament_id)
+    tournament = get_object_or_404(Tournament, id=tournament_id)
+
+    context = {
+        'restaurants': restaurants,
+        'tournament': tournament,
+    }
+
+    return render(request, 'restaurants/restaurants_by_tournament.html', context)
