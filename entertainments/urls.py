cf1669608396ae7af43ef1bd5465119016de5b0d
Jose <reyesjl.nine@hotmail.com>
2024-02-01 20:50:38 +0000

    all views and styles for object_by_tournament, and object_by_location; and object index;

--- entertainments/urls.py
+++ entertainments/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'entertainments'
 urlpatterns = [
+    path('entertainments/', views.index, name='index'),
     path('entertainments/<int:tournament_id>', views.entertainments_by_tournament, name='entertainments_by_tournament'),
+    path('entertainments/<str:city>/<str:state>/', views.entertainments_by_location, name='entertainments_by_location'),
 ]
--- entertainments/views.py
+++ entertainments/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from locations.models import Location
 from .models import Entertainment
 from tournaments.models import Tournament
+
+def index(request):
+    unique_city_states = Location.get_unique_city_state_combinations()
+    return render(request, 'entertainments/index.html', {'unique_city_states':unique_city_states})
 
 def entertainments_by_tournament(request, tournament_id):
     entertainments = Entertainment.objects.filter(tournament_id=tournament_id)
@@ -13,4 +18,14 @@ def entertainments_by_tournament(request, tournament_id):
     
     return render(request, 'entertainments/entertainments_by_tournament.html', context)
 
+def entertainments_by_location(request, city, state):
+    locations = Location.objects.filter(city=city, state=state)
+    entertainments = Entertainment.objects.filter(location__in=locations)
 
+    context = {
+        'city': city,
+        'state': state,
+        'entertainments': entertainments,
+    }
+    
+    return render(request, 'entertainments/entertainments_by_location.html', context)
