b4872a608beabcdb893552e5ecaa4ab13956a542
Jose <reyesjl.nine@hotmail.com>
2024-02-01 20:00:45 +0000

    add main restaurants page; listed by city and state;

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'restaurants'
 urlpatterns = [
+    path('restaurants/', views.index, name='index'),
     path('restaurants/<int:tournament_id>', views.restaurants_by_tournament, name='restaurants_by_tournament'),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from locations.models import Location
 from .models import Restaurant
 from tournaments.models import Tournament
+
+def index(request):
+    unique_city_states = Location.get_unique_city_state_combinations()
+    return render(request, 'restaurants/index.html', {'unique_city_states':unique_city_states})
 
 def restaurants_by_tournament(request, tournament_id):
     restaurants = Restaurant.objects.filter(tournament_id=tournament_id)
