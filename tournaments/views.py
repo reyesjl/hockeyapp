012b03f57bdec01d4f25c9a560fd5968651291c9
Jose <reyesjl.nine@hotmail.com>
2024-02-20 20:19:40 +0000

    add app name to succes redirect in form submit view;

--- tournaments/views.py
+++ tournaments/views.py
@@ -100,7 +100,7 @@ def tournament_review(request, tournament_id):
             review = form.save(commit=False)
             review.tournament = tournament
             review.save()
-            return redirect('short_review_success')  # Redirect to a success page
+            return redirect('tournaments:short_review_success')  # Redirect to a success page
     else:
         form = TournamentReviewForm()
     return render(request, 'tournaments/tournament_review.html', {'form': form})
@@ -113,7 +113,7 @@ def tournament_short_review(request, tournament_id):
             review = form.save(commit=False)
             review.tournament = tournament
             review.save()
-            return redirect('short_review_success')  # Redirect to a success page
+            return redirect('tournaments:short_review_success')  # Redirect to a success page
     else:
         form = TournamentShortReviewForm()
     return render(request, 'tournaments/tournament_short_review.html', {'form': form})
