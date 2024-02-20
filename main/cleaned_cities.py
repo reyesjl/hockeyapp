ee289edf85ecafba78b72aff222aab6ef9c319fe
reyesjl <reyesjl.nine@hotmail.com>
2024-02-19 19:52:06 -0500

    added major US cities

---
+++ main/cleaned_cities.py
@@ -0,0 +1,666 @@
+New York
+NY
+Los Angeles
+CA
+Chicago
+IL
+Houston
+TX
+Phoenix
+AZ
+Philadelphia
+PA
+San Antonio
+TX
+San Diego
+CA
+Dallas
+TX
+Austin
+TX
+Jacksonville
+FL
+San Jose
+CA
+Fort Worth
+TX
+Columbus
+OH
+Charlotte
+NC
+Indianapolis
+IN
+San Francisco
+CA
+Seattle
+WA
+Denver
+CO
+Oklahoma City
+OK
+Nashville
+TN
+El Paso
+TX
+Washington
+DC
+Las Vegas
+NV
+Boston
+MA
+Portland
+OR
+Louisville
+KY
+Memphis
+TN
+Detroit
+MI
+Baltimore
+MD
+Milwaukee
+WI
+Albuquerque
+NM
+Tucson
+AZ
+Fresno
+CA
+Sacramento
+CA
+Mesa
+AZ
+Kansas City
+MO
+Atlanta
+GA
+Colorado Springs
+CO
+Omaha
+NE
+Raleigh
+NC
+Virginia Beach
+VA
+Long Beach
+CA
+Miami
+FL
+Oakland
+CA
+Minneapolis
+MN
+Tulsa
+OK
+Bakersfield
+CA
+Tampa
+FL
+Wichita
+KS
+Arlington
+TX
+Aurora
+CO
+New Orleans
+LA
+Cleveland
+OH
+Anaheim
+CA
+Honolulu
+HI
+Henderson
+NV
+Stockton
+CA
+Riverside
+CA
+Lexington
+KY
+Corpus Christi
+TX
+Orlando
+FL
+Irvine
+CA
+Cincinnati
+OH
+Santa Ana
+CA
+Newark
+NJ
+Saint Paul
+MN
+Pittsburgh
+PA
+Greensboro
+NC
+Lincoln
+NE
+Durham
+NC
+Plano
+TX
+Anchorage
+AK
+Jersey City
+NJ
+St. Louis
+MO
+Chandler
+AZ
+North Las Vegas
+NV
+Chula Vista
+CA
+Buffalo
+NY
+Gilbert
+AZ
+Reno
+NV
+Madison
+WI
+Fort Wayne
+IN
+Toledo
+OH
+Lubbock
+TX
+St. Petersburg
+FL
+Laredo
+TX
+Irving
+TX
+Chesapeake
+VA
+Glendale
+AZ
+Winston-Salem
+NC
+Scottsdale
+AZ
+Garland
+TX
+Boise
+ID
+Norfolk
+VA
+Port St. Lucie
+FL
+Spokane
+WA
+Richmond
+VA
+Fremont
+CA
+Huntsville
+AL
+Tacoma
+WA
+Baton Rouge
+LA
+Santa Clarita
+CA
+San Bernardino
+CA
+Hialeah
+FL
+Frisco
+TX
+Modesto
+CA
+Cape Coral
+FL
+Fontana
+CA
+Moreno Valley
+CA
+Des Moines
+IA
+Rochester
+NY
+Fayetteville
+NC
+Yonkers
+NY
+McKinney
+TX
+Worcester
+MA
+Salt Lake City
+UT
+Little Rock
+AR
+Columbus
+GA
+Augusta
+GA
+Sioux Falls
+SD
+Grand Prairie
+TX
+Tallahassee
+FL
+Amarillo
+TX
+Oxnard
+CA
+Peoria
+AZ
+Overland Park
+KS
+Montgomery
+AL
+Birmingham
+AL
+Grand Rapids
+MI
+Knoxville
+TN
+Vancouver
+WA
+Huntington Beach
+CA
+Providence
+RI
+Brownsville
+TX
+Glendale
+CA
+Akron
+OH
+Tempe
+AZ
+Newport News
+VA
+Chattanooga
+TN
+Mobile
+AL
+Fort Lauderdale
+FL
+Cary
+NC
+Shreveport
+LA
+Ontario
+CA
+Eugene
+OR
+Aurora
+IL
+Elk Grove
+CA
+Salem
+OR
+Santa Rosa
+CA
+Clarksville
+TN
+Rancho Cucamonga
+CA
+Oceanside
+CA
+Springfield
+MO
+Pembroke Pines
+FL
+Garden Grove
+CA
+Fort Collins
+CO
+Lancaster
+CA
+Palmdale
+CA
+Murfreesboro
+TN
+Salinas
+CA
+Corona
+CA
+Killeen
+TX
+Hayward
+CA
+Paterson
+NJ
+Macon
+GA
+Lakewood
+CO
+Alexandria
+VA
+Roseville
+CA
+Surprise
+AZ
+Springfield
+MA
+Charleston
+SC
+Kansas City
+KS
+Sunnyvale
+CA
+Bellevue
+WA
+Hollywood
+FL
+Denton
+TX
+Escondido
+CA
+Joliet
+IL
+Naperville
+IL
+Bridgeport
+CT
+Savannah
+GA
+Mesquite
+TX
+Pasadena
+TX
+Rockford
+IL
+Pomona
+CA
+Jackson
+MS
+Olathe
+KS
+Gainesville
+FL
+McAllen
+TX
+Syracuse
+NY
+Waco
+TX
+Visalia
+CA
+Thornton
+CO
+Torrance
+CA
+Fullerton
+CA
+Columbia
+SC
+Lakewood
+NJ
+New Haven
+CT
+Hampton
+VA
+Miramar
+FL
+Victorville
+CA
+Warren
+MI
+West Valley City
+UT
+Cedar Rapids
+IA
+Stamford
+CT
+Orange
+CA
+Dayton
+OH
+Midland
+TX
+Kent
+WA
+Elizabeth
+NJ
+Pasadena
+CA
+Carrollton
+TX
+Coral Springs
+FL
+Sterling Heights
+MI
+Fargo
+ND
+Lewisville
+TX
+Meridian
+ID
+Norman
+OK
+Palm Bay
+FL
+Athens
+GA
+Columbia
+MO
+Abilene
+TX
+Pearland
+TX
+Santa Clara
+CA
+Round Rock
+TX
+Topeka
+KS
+Allentown
+PA
+Clovis
+CA
+Simi Valley
+CA
+College Station
+TX
+Thousand Oaks
+CA
+Vallejo
+CA
+Concord
+CA
+Rochester
+MN
+Arvada
+CO
+Lafayette
+LA
+Independence
+MO
+West Palm Beach
+FL
+Hartford
+CT
+Wilmington
+NC
+Lakeland
+FL
+Billings
+MT
+Ann Arbor
+MI
+Fairfield
+CA
+Berkeley
+CA
+Richardson
+TX
+North Charleston
+SC
+Cambridge
+MA
+Broken Arrow
+OK
+Clearwater
+FL
+West Jordan
+UT
+Evansville
+IN
+League City
+TX
+Antioch
+CA
+Manchester
+NH
+High Point
+NC
+Waterbury
+CT
+Westminster
+CO
+Richmond
+CA
+Carlsbad
+CA
+Las Cruces
+NM
+Murrieta
+CA
+Lowell
+MA
+Provo
+UT
+Springfield
+IL
+Elgin
+IL
+Odessa
+TX
+Lansing
+MI
+Pompano Beach
+FL
+Beaumont
+TX
+Temecula
+CA
+Gresham
+OR
+Allen
+TX
+Pueblo
+CO
+Everett
+WA
+South Fulton
+GA
+Peoria
+IL
+Nampa
+ID
+Tuscaloosa
+AL
+Miami Gardens
+FL
+Santa Maria
+CA
+Downey
+CA
+Concord
+NC
+Ventura
+CA
+Costa Mesa
+CA
+Sugar Land
+TX
+Menifee
+CA
+Tyler
+TX
+Sparks
+NV
+Greeley
+CO
+Rio Rancho
+NM
+Sandy Springs
+GA
+Dearborn
+MI
+Jurupa Valley
+CA
+Edison
+NJ
+Spokane Valley
+WA
+Hillsboro
+OR
+Davie
+FL
+Green Bay
+WI
+Centennial
+CO
+Buckeye
+AZ
+Boulder
+CO
+Goodyear
+AZ
+El Monte
+CA
+West Covina
+CA
+Brockton
+MA
+New Braunfels
+TX
+El Cajon
+CA
+Edinburg
+TX
+Renton
+WA
+Burbank
+CA
+Inglewood
+CA
+Rialto
+CA
+Lee's Summit
+MO
+Bend
+OR
+Woodbridge
+NJ
+South Bend
+IN
+Wichita Falls
+TX
+St. George
+UT
+Fishers
+IN
+Carmel
+IN
+Vacaville
+CA
+Quincy
+MA
+Conroe
+TX
+Chico
+CA
+San Mateo
+CA
+Lynn
+MA
+Albany
+NY
+Hesperia
+CA
+New Bedford
+MA
+Davenport
+IA
+Daly City
+CA
