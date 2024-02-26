600b846b5653423030880286e7a1635b9716776a
Jose <reyesjl.nine@hotmail.com>
2024-02-26 15:48:04 +0000

    add company to admin;

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Tournament, TournamentMetadata, Rink
 
+admin.site.register(Company)
 admin.site.register(Tournament)
 admin.site.register(TournamentMetadata)
 admin.site.register(Rink)
