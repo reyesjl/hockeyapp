4495e933ee6636541078e1fe55c57f3a5af952ef
Jose <reyesjl.nine@hotmail.com>
2024-02-26 16:02:10 +0000

    add filters to tournament admin;

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import Tournament, TournamentMetadata, Rink, Company
 
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = ['company', 'name','majorcity']  # Display fields in the list view
+    list_filter = ['company__name']  # Enable filtering by company name
+
+class TournamentMetadataAdmin(admin.ModelAdmin):
+    list_display = ['tournament', 'count_rinks', 'count_thumbs_up', 'count_thumbs_down',
+                    'rating_reffing', 'rating_comms', 'rating_hotels',
+                    'parking_size', 'parking_valet', 'parking_cost', 'stay_and_play']  
+    list_filter = ['tournament__company__name']  # Enable filtering by company name
+
 admin.site.register(Company)
-admin.site.register(Tournament)
-admin.site.register(TournamentMetadata)
+admin.site.register(Tournament, TournamentAdmin)
+admin.site.register(TournamentMetadata, TournamentMetadataAdmin)
 admin.site.register(Rink)
