08c3d1c5345fd2f5542f9ea348373191d2172806
reyesjl <reyesjl.nine@hotmail.com>
2024-03-24 13:13:14 -0400

    green theme to activity type;

--- tournament/admin.py
+++ tournament/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Location, Tournament, TournamentCompany
 
 class TournamentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'address', 'reff_rating', 'comms_rating', 'parking_size', 'parking_valet', 'parking_cost', 'stay_and_play', 'extended_checkout', 'draft_status')
-    list_filter = ('draft_status', 'stay_and_play', 'extended_checkout')
+    list_display = ('name', 'address', 'company', 'reff_rating', 'comms_rating', 'parking_size', 'parking_valet', 'parking_cost', 'stay_and_play', 'extended_checkout', 'draft_status')
+    list_filter = ('draft_status', 'stay_and_play', 'extended_checkout', 'company')
     search_fields = ('name','address',)
 
 class TournamentCompanyAdmin(admin.ModelAdmin):
