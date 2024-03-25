875cc56c631e5050fabf3c2a070cd6c3c372fe0a
reyesjl <reyesjl.nine@hotmail.com>
2024-03-25 16:17:29 -0400

    remove object list_filter from admin;

--- review/admin.py
+++ review/admin.py
@@ -4,19 +4,19 @@ from .models import TournamentReview, RestaurantReview, EntertainmentReview
 class TournamentReviewAdmin(admin.ModelAdmin):
     list_display = ['tournament', 'author', 'date', 'rating']
     search_fields = ['tournament__name', 'author']
-    list_filter = ['tournament', 'date']
+    list_filter = ['date']
     date_hierarchy = 'date'
 
 class RestaurantReviewAdmin(admin.ModelAdmin):
     list_display = ['restaurant', 'author', 'date', 'meal_quality', 'service_quality']
     search_fields = ['restaurant__name', 'author']
-    list_filter = ['restaurant', 'date']
+    list_filter = ['date']
     date_hierarchy = 'date'
 
 class EntertainmentReviewAdmin(admin.ModelAdmin):
     list_display = ['entertainment', 'author', 'date', 'service_rating']
     search_fields = ['entertainment__name', 'author']
-    list_filter = ['entertainment', 'date']
+    list_filter = ['date']
     date_hierarchy = 'date'
 
 admin.site.register(TournamentReview, TournamentReviewAdmin)
