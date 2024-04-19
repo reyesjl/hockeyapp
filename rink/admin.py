ac9a185933534d4eae810c5feb9f2e2623762ccf
reyesjl <reyesjl.nine@hotmail.com>
2024-04-19 15:13:04 -0400

    add payment modes to admin page;

--- rink/admin.py
+++ rink/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Rink, ParkingAvailability
+from .models import Rink, ParkingAvailability, PaymentModes
 
 admin.site.register(Rink)
+admin.site.register(PaymentModes)
 admin.site.register(ParkingAvailability)
